e84845b939462dc2a4b925f5492f4bfe48cf759b
rgmanikandan07 <rgmanikandan07@gmail.com>
2024-11-02 21:13:55 +0530

    emc

--- emc.js
+++ emc.js
@@ -109,3 +109,14 @@
 // console.log(one.includes("ow"));
 // const second = "hello";
 // console.log(second.includes("ll"));
+
+
+
+function factorial(n) {
+    if (n === 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+console.log(factorial(5));
