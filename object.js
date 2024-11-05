3e2269d9fe3e02be8855594b5717f5a7ea017378
rgmanikandan07 <rgmanikandan07@gmail.com>
2024-11-05 10:15:07 +0530

    obj

---
+++ object.js
@@ -0,0 +1,28 @@
+// 1.Create and Access Object:
+// Create an object representing a book with properties like title, author, and year. Access and modify these properties.
+let book = {
+    title: "Psycology of Money",
+    author: "Morgan",
+    year: 2020
+}
+console.log(book.title);
+console.log(book.author);
+console.log(book.year);
+
+// 2.Object Methods:
+// Add a method to the book object that prints a summary of the book (title and author).  
+let book2 = {
+        title: "Psycology of Money",
+        author: "Morgan",
+        summary: "Add a method to the book object that prints a summary of the book (title and author).  "
+    }
+    console.log(book2.summary);
+
+// 3.Object Destructuring:
+// Use object destructuring to extract the title and year of the book from the object.
+
+// 4.Object Spread:
+// Create another object with additional properties. Use the spread operator to merge it with the original book object.
+
+// 5.Iterate Properties:
+// Write a for...in loop to iterate over the properties of the book object and log them to the console.
