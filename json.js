3c612165800663291aa92888ba13719f5aabbdb7
rgmanikandan07 <rgmanikandan07@gmail.com>
2024-11-14 15:38:15 +0530

    jsontask

--- json.js
+++ json.js
@@ -1,13 +1,11 @@
-// 1. Create a JSON object that contains information about a book. It should include the title, author,
-//    publication year, genre, and an array of reviews.
+// 1. Create a JSON object that contains information about a book. It should include the title, author, publication year, genre, and an array of reviews.
 const book = {
     "title": "ABC",
     "author": "xy",
     "publication year": 2020,
     "reviews": [
         {
-            "first": "Good",
-            "last": "Bad"
+            "review": "***"
         }
     ]
 }
@@ -51,3 +49,22 @@ const nested = {
 console.log(nested.Address[0].city);
 
 
+// 5.  Modify a JSON Array
+let modify = [
+    { "name": "John", "age": 28 },
+    { "name": "Anna", "age": 22 },
+    { "name": "Peter", "age": 35 }
+];
+
+let newItem = { "name": "mani", "age": 23 };
+let addItem = [...modify, newItem];
+console.log(addItem);
+
+let removeItem = { "name": "Anna", "age": 22 };
+delete removeItem.name
+console.log(removeItem);
+
+let changeAge = 40;
+let updatePerson = { "name": "Peter", "age": 35 };
+let newChange = modify.map(item => item.age === updatePerson.age ? { ...updatePerson, age: changeAge } : item);
+console.log(newChange);
